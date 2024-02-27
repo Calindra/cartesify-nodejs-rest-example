1b49420473610190cbcba57071175578371144d1
Fabio Oshiro <fabio.oshiro@calindra.com.br>
2024-02-27 15:32:37 -0300

    feat: exec voucher

--- README.md
+++ README.md
@@ -5,22 +5,27 @@
 ### With Nonodo
 
 Start nonodo
+
 ```shell
 nonodo
 ```
 
 Start the backend
+
 ```shell
 node src/app.js
 ```
 
 ### With Sunodo
+
 Build:
+
 ```shell
 sunodo build
 ```
 
 Success output:
+
 ```shell
  => exporting to image                                                                                                                                                        0.2s
  => => exporting layers                                                                                                                                                       0.2s
@@ -61,6 +66,40 @@ Storing machine: please wait
 ## Frontend
 
 Start the frontend:
+
 ```shell
 npm run dev
 ```
+
+## How to advance the epoch
+
+```shell
+ETH_RPC_URL=http://localhost:8545 cast rpc evm_increaseTime 2592000
+```
+
+## How to configure the DApp address
+
+Run the command:
+
+```shell
+sunodo address-book
+```
+
+Output:
+
+```shell
+ Contract            Address                                    
+ ─────────────────── ────────────────────────────────────────── 
+ CartesiDAppFactory  0x7122cd1221C20892234186facfE8615e6743Ab02 
+ DAppAddressRelay    0xF5DE34d6BbC0446E2a45719E718efEbaaE179daE 
+ ERC1155BatchPortal  0xedB53860A6B52bbb7561Ad596416ee9965B055Aa 
+ ERC1155SinglePortal 0x7CFB0193Ca87eB6e48056885E026552c3A941FC4 
+ ERC20Portal         0x9C21AEb2093C32DDbC53eEF24B873BDCd1aDa1DB 
+ ERC721Portal        0x237F8DD094C0e47f4236f12b4Fa01d6Dae89fb87 
+ EtherPortal         0xFfdbe43d4c855BF7e0f105c400A50857f53AB044 
+ InputBox            0x59b22D57D4f067708AB0c00552767405926dc768 
+ SunodoToken         0xf795b3D15D47ac1c61BEf4Cc6469EBb2454C6a9b 
+ CartesiDApp         0x70ac08179605AF2D9e75782b8DEcDD3c22aA4D0C 
+```
+
+The DApp Address is 0x70ac08179605AF2D9e75782b8DEcDD3c22aA4D0C
--- frontend/src/cartesi/ERC1155Deposit.tsx
+++ frontend/src/cartesi/ERC1155Deposit.tsx
@@ -1,7 +1,9 @@
 import { FetchFun } from "@calindra/cartesify/src/cartesify/FetchLikeClient"
-import { ERC1155BatchPortal__factory, ERC1155SinglePortal__factory, IERC1155__factory } from "@cartesi/rollups"
+import { ERC1155BatchPortal__factory, ERC1155SinglePortal__factory, IERC1155__factory, CartesiDApp__factory, DAppAddressRelay } from "@cartesi/rollups"
 import { Signer } from "ethers"
 import { useEffect, useState } from "react"
+import { Voucher } from "./model/Voucher"
+import { VoucherService } from "./services/VoucherService"
 
 type ERC1155DepositProps = {
     dappAddress: string
@@ -15,7 +17,8 @@ export default function ERC1155Deposit({ getSigner, dappAddress, fetch }: ERC115
     const [value, setValue] = useState(1)
 
     const [batchSize, setBatchSize] = useState(2)
-    const [batch, setBatch] = useState<{ tokenId: string, value: number }[]>([])
+    const [batch, setBatch] = useState<{ tokenId: string, value: number, balance?: number }[]>([])
+    const [vouchers, setVouchers] = useState<Voucher[]>([])
 
     function setErc1155Address(address: string) {
         _setErc1155Address(address)
@@ -37,10 +40,9 @@ export default function ERC1155Deposit({ getSigner, dappAddress, fetch }: ERC115
     async function batchDeposit() {
         const signer = await getSigner()
         // you could check this address by executing `sunodo run --verbose`
-        console.log(await signer.getAddress())
+        // const userAddress = await signer.getAddress()
         const portalAddress = '0xedB53860A6B52bbb7561Ad596416ee9965B055Aa'
         const contract = IERC1155__factory.connect(erc1155address, signer)
-
         const approve = await contract.setApprovalForAll(portalAddress, true)
         console.log('approve', approve)
 
@@ -78,11 +80,74 @@ export default function ERC1155Deposit({ getSigner, dappAddress, fetch }: ERC115
         const newBatch = batch.slice(0, newSize)
         for (let i = newBatch.length; i < newSize; i++) {
             newBatch.push({
-                tokenId: `${i + 1}`,
+                tokenId: `${i}`,
                 value: 1
             })
         }
-        setBatch(newBatch)
+        loadBalances(newBatch)
+    }
+
+    async function loadBalances(batch: { tokenId: string; value: number; balance?: number }[]) {
+        const signer = await getSigner()
+        // you could check this address by executing `sunodo run --verbose`
+        const userAddress = await signer.getAddress()
+        // const userAddress = '0x70ac08179605AF2D9e75782b8DEcDD3c22aA4D0C'
+        const contract = IERC1155__factory.connect(erc1155address, signer)
+        const balances = await contract.balanceOfBatch(batch.map(() => userAddress), batch.map(b => b.tokenId))
+        balances.forEach((b, i) => {
+            batch[i].balance = b.toString()
+        })
+        setBatch(batch)
+    }
+    
+
+    async function init() {
+        const res = await VoucherService.findAll()
+        const vouchers = res.data.vouchers.edges.map((e: any) => e.node)
+        console.log(vouchers)
+        setVouchers(vouchers)
+    }
+
+    useEffect(() => {
+        init()
+    }, [])
+
+    /*
+     | 0xF5B2d8c81cDE4D6238bBf20D3D77DB37df13f735 Bitmask
+     | 0xB634F716BEd5Dd5A2b9a91C92474C499e50Cb27D CartesiMathV2
+     | 0x33436035441927Df1a73FE3AAC5906854632e53d MerkleV2
+     | 0x3F8FdcD1B0F421D817BF58C96b7C91B98100B450 UnrolledCordic
+     | 0x59b22D57D4f067708AB0c00552767405926dc768 InputBox
+     | 0xFfdbe43d4c855BF7e0f105c400A50857f53AB044 EtherPortal
+     | 0x9C21AEb2093C32DDbC53eEF24B873BDCd1aDa1DB ERC20Portal
+     | 0x237F8DD094C0e47f4236f12b4Fa01d6Dae89fb87 ERC721Portal
+     | 0x7CFB0193Ca87eB6e48056885E026552c3A941FC4 ERC1155SinglePortal
+     | 0xedB53860A6B52bbb7561Ad596416ee9965B055Aa ERC1155BatchPortal
+     | 0xF5DE34d6BbC0446E2a45719E718efEbaaE179daE DAppAddressRelay
+     | 0x7122cd1221C20892234186facfE8615e6743Ab02 CartesiDAppFactory
+     | 0x5050F233F2312B1636eb7CF6c7876D9cC6ac4785 Authority
+     | 0x4FF8BD9122b7D91d56Dd5c88FE6891Fb3c0b5281 History
+     | 0xae7f61eCf06C65405560166b259C54031428A9C4 SunodoToken
+     | 0x519421Bd7843e0D1E2F280490962850e31c86087 AuthorityFactory
+     | 0x5314730B6285B9B026FAaD759aa39A9415eB874c PayableDAppSystem
+     */
+    async function executeVoucher(voucher: Voucher) {
+        const address = '0x70ac08179605AF2D9e75782b8DEcDD3c22aA4D0C'
+        const signer = await getSigner()
+        const cartesiDApp = CartesiDApp__factory.connect(address, signer)
+        // cartesiDApp.addListener('VoucherExecuted', (...args) => {
+        //     console.log('VoucherExecuted', args)
+        // })
+        const executed = await cartesiDApp.wasVoucherExecuted(voucher.input.index, voucher.index)
+        if (executed) {
+            console.log('Voucher was executed!!!')
+        } else {
+            console.log('executing voucher...')
+            const tx = await cartesiDApp.executeVoucher(voucher.destination, voucher.payload, voucher.proof)
+            console.log(tx)
+            const res = await (tx as any).wait()
+            console.log('Executed!', res)
+        }
     }
 
     useEffect(() => updateBatchSize(batchSize), [])
@@ -105,18 +170,32 @@ export default function ERC1155Deposit({ getSigner, dappAddress, fetch }: ERC115
                     <div key={`${i}`} style={{ border: '1px solid #444', padding: '3px', marginTop: '3px' }}>
                         Token ID: <input value={item.tokenId} onChange={(e) => {
                             batch[i].tokenId = e.target.value;
-                            setBatch([...batch])
+                            loadBalances([...batch])
                         }} /><br />
                         Value: <input value={item.value} onChange={(e) => {
                             batch[i].value = +e.target.value;
                             setBatch([...batch])
                         }} /><br />
+                        Balance: {batch[i].balance ?? "0"}<br />
                     </div>
                 )
             })}
             <button onClick={batchDeposit}>Deposit</button>
 
             <button onClick={batchWithdraw}>Withdraw</button>
+
+            <h3>Vouchers</h3>
+            {vouchers.map((voucher, i) => {
+                return (
+                    <div key={`${i}`}>
+                        {voucher.destination}
+                        <button onClick={async () => {
+                            await executeVoucher(voucher)
+                        }}>Execute</button>
+                    </div>
+                )
+            })}
         </div>
     )
 }
+
